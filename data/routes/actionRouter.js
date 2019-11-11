9c2c7d008f1a17bc6b64a5472e51668053ecb2f3
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-11-10 19:41:51 -0800

    Added .DELETE to actions

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -52,6 +52,16 @@ router.get("/:id", validateId, (req, res) => {
       });
   });
 
+  router.delete("/:id", validateId, (req, res) => {
+    db.remove(req.params.id)
+      .then(deleted => {
+        res.status(200).json(deleted);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Error deleting action", err });
+      });
+  });
+
   
   // ***** MIDDLEWARE *****
   
